6519bab200423d29aef99e3d1de1b1976eefd0e5
Kitness Chen <rdchenrui@gmail.com>
2014-12-02 13:19:32 +0800

    change css, html, fonts and add index

---
+++ web/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //sign up
+    $('button#signup').click(function(){
+        location.href="signup.jsp";
+    })
+    
+    //login
+    $('button#login').click(function(){
+        location.href="login.jsp";
+    })
+})
