55f51ffd730d3b06b8eb1be1fb079200822c0154
whd <5522189774@qq.com>
2014-12-04 09:31:42 +0800

    fix signup.js signup.jsp

--- out/artifacts/TeamWork_war_exploded/pages/js/signup.js
+++ out/artifacts/TeamWork_war_exploded/pages/js/signup.js
@@ -1,19 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //sign up
-    $('button').click(function(){
-        var email = $("email").val();
+    $('button').click(function () {
+        var email = $("#email").val();
         var username = $("#username").val();
         var password = $("#password").val();
-        $.ajax({
-            url: '/signup',
-            type: 'post',
-            dataType: 'json',
-            data:{'action': 'signin', 'email': email, 'username': username, 'password': password},
-            success: function(msg){
-                alert(msg);
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown){
-                alert(errorThrown);}
-            });
+        var data = {
+            'email': email,
+            'username': username,
+            'password': password
+        };
+        $.post('/signup', data, function (msg) {
+            document.body.innerHTML = msg;
+        });
+//        $.ajax({
+//            url: '/signup',
+//            type: 'post',
+//            dataType: 'json',
+//            data: {'action': 'signup', 'email': email, 'username': username, 'password': password},
+//            success: function (msg) {
+//                alert(msg);
+//            },
+//            error: function (XMLHttpRequest, textStatus, errorThrown) {
+//                alert(errorThrown);
+//            }
+//        });
     });
 });
